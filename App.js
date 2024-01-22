bba343c4d4ce109db64108ffce351a914bcaace1
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-01-23 00:06:46 +0700

    Nhan pussh code food detail
    Nhan pussh code food detail

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import Welcome from './src/screens/Welcome/Welcome'
 import WelcomeToFood from './src/screens/Welcome/WelcomeToFood'
 import LoginScreen from './src/screens/Login/LoginScreen'
 import SignUpScreen from './src/screens/Login/SignUpScreen'
+// <<<<<<< Updated upstream
 import Profiles from './src/screens/Page/Profiles'
 import Card from './src/screens/Page/Cart'
 import ItemCard from './src/components/ItemCart'
 import MyOrder from './src/screens/Page/MyOrder'
 import ItemOrder1 from './src/components/ItemOrder1'
 import ItemOrder2 from './src/components/ItemOrder2'
+// =======
+import Food_Detail from './src/screens/Page/Food_Detail'
+// >>>>>>> Stashed changes
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
+      <Food_Detail/>
       {/* <Welcome/> */}
       {/* <WelcomeToFood/> */}
       {/* <LoginScreen/> */}
@@ -21,7 +26,7 @@ const App = () => {
       {/* <Profiles/> */}
       {/* <Cart/> */}
       {/* <ItemCart/> */}
-      <MyOrder/>
+      {/* <MyOrder/> */}
       {/* <ItemOrder1/> */}
       {/* <ItemOrder2/> */}
     </SafeAreaView>
